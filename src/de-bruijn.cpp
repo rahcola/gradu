405800048171b90d8911d1fca76bec120014a55f
Jani Rahkola <jani.rahkola@iki.fi>
2015-03-19 20:32:07 +0200

    tää ei toimi, T. joulupukki

--- src/de-bruijn.cpp
+++ src/de-bruijn.cpp
@@ -196,6 +196,31 @@ getArc(BidirectionalBWTIndex index,
                          index.forward.wavelet_tree.rank(j, c));
 }
 
+namespace aux{
+template<std::size_t...> struct seq{};
+
+template<std::size_t N, std::size_t... Is>
+struct gen_seq : gen_seq<N-1, N-1, Is...>{};
+
+template<std::size_t... Is>
+struct gen_seq<0, Is...> : seq<Is...>{};
+
+template<class Ch, class Tr, class Tuple, std::size_t... Is>
+void print_tuple(std::basic_ostream<Ch,Tr>& os, Tuple const& t, seq<Is...>){
+  using swallow = int[];
+  (void)swallow{0, (void(os << (Is == 0? "" : ", ") << std::get<Is>(t)), 0)...};
+}
+} // aux::
+
+template<class Ch, class Tr, class... Args>
+auto operator<<(std::basic_ostream<Ch, Tr>& os, std::tuple<Args...> const& t)
+    -> std::basic_ostream<Ch, Tr>&
+{
+  os << "(";
+  aux::print_tuple(os, t, aux::gen_seq<sizeof...(Args)>());
+  return os << ")";
+}
+
 int main(int argc, char *argv[]) {
   typedef BidirectionalBWTIndex::interval interval;
   typedef BidirectionalBWTIndex::value_type value_type;
@@ -223,8 +248,7 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  for (unsigned int i = 0; i < first.size(); i++) {
-    std::cout << first[i] << std::endl;
-  }
+  std::cout << getArc(index, std::make_tuple(7, 9), index.forward.char2comp['C'])
+            << std::endl;
   return 0;
 }
